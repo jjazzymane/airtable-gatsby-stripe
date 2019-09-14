4557d0f924cd167cfe09dde703c4b0ac9b9ab963
jjazzymane <jjazzymane@live.com>
2019-09-14 11:27:07 -0400

    deleted commented code

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,71 +1,3 @@
-// Foreach plan retrieve the product name with the given product id. 
-
-// exports.onCreateNode = async ({ node, actions, createNodeId, createContentDigest }) => {
-//     if (node.id !== node.product) {
-//         return;
-//     }
-   
-    
-//     const data = { 
-//         id: createNodeId(`CoachClaraPlans-${node.product}`),
-//         amount: node.amount
-//     }
-
-//     actions.onCreateNode({
-//         parent: node.product,
-//         ...data,
-//         internal: {
-//             type: 'CoachClaraPlans',
-//             contentDigest: createContentDigest(data),
-//         }
-//     }) 
-
-//     /* if (node.internal.type === 'StripeProduct'){ 
-//         const productData = {
-//             id: createNodeId(`CoachClaraProduct-${node.id}`),
-//             name: node.id,
-//         }
-//         return productData
-
-//     };
-
-//     const data = {
-//         id: createNodeId(`Subscriptions-${node.product}`),
-//         nickname: node.nickname,
-//         productId: node.product,
-//         amount: node.amount, 
-//         currency: node.currency,
-//     }
-
-
-//     actions.createNode({
-//         parent: node.product,
-//         ...data,
-//         internal: {
-//             type: 'CoachClaraSubscriptions',
-//             contentDigest: createContentDigest(data),
-//         }
-//     })
-
-//     const {node: products} = productData 
-//     actions.createNodeField({
-//         node, 
-//         name: `name`,
-//         value: product.
-//     }) */
-// }
-
-// exports.createSchemaCustomization = ({ actions }) => {
-//     const { createTypes } = actions
-//     const typeDefs = `
-        
-//         type StripeProduct implements Node & StripePlanGroupConnection {
-//             name: String
-//         }
-//     `
-//     createTypes(typeDefs)
-// }
-
 exports.createResolvers = ({ 
     actions, 
     cache,
@@ -83,13 +15,9 @@ exports.createResolvers = ({
                 type: 'String',
                 resolve(source, _args, context, _info) {
                     const stripeProduct = getNodesByType(`StripeProduct`)
+                    
                     //return source.fieldValue
-                    /* for (yaya in stripeProduct) {
-                        if (stripeProduct[yaya].id === source.fieldValue ) {
-                            return stripeProduct[yaya].id
-                        }
-                    } */
-
+                    
                     for (yaya in stripeProduct) { 
                         if (stripeProduct[yaya].id === source.fieldValue ) { 
                             return stripeProduct[yaya].name
@@ -99,21 +27,6 @@ exports.createResolvers = ({
             }
 
         },
-        /* AirtableConnection: {
-            CoachClaraAirtable: {
-                type: 'Airtable',
-                resolve(source, _args, context, _info) {
-                    const airtableData = getNodesByType(`AirtableData`)
-                    const allNodes = getNodes()
-                    for (these in allNodes) {
-                        if (allNodes[these].internal.type === `Airtable`|| allNodes[these].queryName === `Products-and-plans`) {
-                            return allNodes[these].data.TINY_TITLE
-                        }
-                    }
-
-                }
-            }
-        } */
     })
 }
 
@@ -121,12 +34,6 @@ exports.onCreateNode = ({ node, actions, getNodes, createNodeId, createContentDi
     if (node.internal.type !== `Airtable`|| node.queryName !== `Products-and-plans`) {
         return;
     }
-    /* const allNodes = getNodes()
-    for (these in allNodes) {
-        if (allNodes[these].internal.type === `Airtable`|| allNodes[these].queryName === `Products-and-plans`) {
-            return console.log(allNodes[these].data.FEATURES)
-        }
-    } */
 
     const data = {
         id: createNodeId(`CoachClaraAirtable-${node.id}`),
@@ -134,15 +41,6 @@ exports.onCreateNode = ({ node, actions, getNodes, createNodeId, createContentDi
         titleProduct: node.data.PROGRAM_TITLE,
         descriptionProgram: node.data.PROGRAM_DESCRIPTION,
         descriptionCost: node.data.COST_DESCRIPTION,
-        //features: node.data.FEATURES.data.FEATURE_NAME,
-        /* plans: {
-            planFULL: node.data.ONE_TIME.data.COST_OPT_FULL,
-            planBWKLY: node.data.BI_WEEKLY.data.COST_OPT_BIWEEKLY,
-            planMONTHLY: node.data.MONTHLY.data.COST_OPT_MTHLY,
-            planGROUP15: node.data.GROUP_15.data.COST_OPT_GROUP,
-            planGROUP25: node.data.GROUP_25.data.COST_OPT_GROUP,
-        } */
-        
     }
     actions.createNode({
         parent: node.id,
@@ -196,23 +94,6 @@ exports.sourceNodes = ({ actions, schema, getNodesByType }) => {
                         }
                         // return the new array 
                         return whtIwant
-                        
-                        // console.log(source.parent); // see if we are getting the source
-                        // console.log('hello??? BITCHESSS'); // see if the resolver is being run at all
-                        // console.log(airtableNode); // see if we are getting any airtalbe nodes
-                        
-
-                    //     const featuresNode = context.nodeModel.getNodeById({
-                    //         id: airtableNode.data.FEATURES___NODE
-                    //     })
-                    //     // console.log('BOUT TO SHOW FEATURES NODES')
-                    //     // console.log(featuresNode)
-
-                    //     // for (deedee in featuresNode) {
-                    //     //     if 
-                    //     // }
-
-                        //return { type: 'String', aghhh: airtableNode.data.FEATURES___NODE}
                     },
                 },
                 // next : put the plans in a wrapper called "plans", it takes a type as well. create a type for
